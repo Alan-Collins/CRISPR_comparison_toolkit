e4d90c898c1663149b80a383e14c8fe7eb025bfd
Alan Collins <alan.collins@bath.edu>
2021-04-29 11:53:09 -0500

    Moved CRISPR finding scripts to directory
    Decided to organize the directory into the steps of a CRISPR analysis. Scripts to find CRISPRs are now in the directory associated with that step.

---
+++ Find_CRISPR_arrays/Process_CRISPR_cas_finder_out.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python3
+
+# AUTHOR		:	ALAN COLLINS
+# VERSION		:	v1
+# DATE			:	2021/1/11
+# DESCRIPTION	:	Script to process json files output by CRISPR-cas finder and summarize them in various human-readable files.
+
+import sys
+import argparse
+import textwrap as _textwrap
+import json
+import os
+import xml.etree.ElementTree as Et
+
+# local packages
+
+import rev_comp
+import hamming_dist
+
+
+
+class LineWrapRawTextHelpFormatter(argparse.RawDescriptionHelpFormatter):
+	"""
+	Short function for argparse that wraps text properly when printing to terminal
+	"""
+	def _split_lines(self, text, width):
+		text = self._whitespace_matcher.sub(' ', text).strip()
+		return _textwrap.wrap(text, width)
+
+parser = argparse.ArgumentParser(
+	description="Script to process json files output by CRISPR-cas finder and summarize them in various human-readable files.",
+	formatter_class=LineWrapRawTextHelpFormatter)
+parser.add_argument(
+	"-i",  dest="indirs", required = False, nargs="+",
+	help="Specify input directories containing output files that were produced by CRISPR-cas finder."
+	)
+parser.add_argument(
+	"-o",  dest="outdir", required = False,
+	help="Specify output directory into which you want the script to place its output files. Default is to output in the current working directory"
+	)
+parser.add_argument(
+    "-r", dest="repeats_file", required = False,
+    help="FASTA format file containing the CRISPR repeats you want to look for. If you don't provide one, this script will look for type 1F, 1E, and 1C repeats from Pseudomonas aeruginosa. These are used to classify arrays and orient them consistently with one another."
+    )
+# parser.add_argument(
+# 	"-p",  dest="outprefix", required = False,
+# 	help="Specify a prefix string you want at the beginning of output file names."
+# 	)
+
+def get_repeat_info(CRISPR_types_dict, repeat):
+
+	best_score = 1000
+	best_match = ''
+	reverse = False
+	for k,v in CRISPR_types_dict.items():
+		score = min(hamming_dist.hamming(repeat, v))
+		if score < best_score:
+			best_score = score
+			best_match = k
+			reverse = False
+
+		score = min(hamming_dist.hamming(rev_comp.rev_comp(repeat), v))
+		if score < best_score:
+			best_score = score
+			best_match = k
+			reverse = True
+
+
+	return best_match, best_score, reverse
+
+def fasta_to_dict(FASTA_file):
+	fasta_dict = {}
+	with open(FASTA_file, 'r') as f:
+		multifasta = f.read()
+	f.close()
+	fastas = multifasta.split(">")
+	trimmed_fastas = []
+	for i in fastas:
+		if len(i) != 0:
+			trimmed_fastas.append(i)
+
+	fastas = trimmed_fastas
+
+	for i in fastas:
+		header = i.split("\n")[0]
+		seq = "".join(i.split("\n")[1:])
+		fasta_dict[header] = seq
+
+	return fasta_dict
+
+
+args = parser.parse_args(sys.argv[1:])
+
+
+if args.repeats_file:
+	CRISPR_types_dict = fasta_to_dict(args.repeats_file)
+else:
+	CRISPR_types_dict = { #Specify the desired orientation and type of all repeats you are interested in here
+	'1E':'GTGTTCCCCACGGGTGTGGGGATGAACCG',
+	'1F':'GTTCACTGCCGTGTAGGCAGCTAAGAAA',
+	'1C':'GTCGCGCCCCGCACGGGCGCGTGGATTGAAAC'
+	}
+
+### Read in CRISPR system Cas protein specifications (stolen from Macsyfinder packaged with CRISPRCasFinder) Files must be stored in ./Data_files/CRISPR_system_defs/ relative to this script's location.
+
+Cas_dict = {}
+
+path_prefix = '/'.join(os.path.realpath(__file__).split('/')[:-1]) + '/'
+definitions_path = path_prefix + "Data_files/CRISPR_system_defs/"
+
+for file in os.listdir(definitions_path):
+	CR_type = file.strip('.xml')
+	Cas_dict[CR_type] = {'n_mandatory' : 0}
+
+	tree = Et.parse(definitions_path + file)
+	root = tree.getroot()
+
+	for child in root:
+		Cas_dict[CR_type][child.attrib["name"]] =  child.attrib["presence"]
+		if child.attrib["presence"] == 'mandatory':
+			Cas_dict[CR_type]['n_mandatory'] += 1
+
+
+results = {}
+Cas_results = {}
+spacers = {}
+nspacers_seen = 1
+arrays = {}
+arrays_encoded = {}
+narrays_seen = 1
+
+
+outdir = args.outdir + '/' if args.outdir[-1] != '/' else args.outdir
+
+for indir in args.indirs:
+	indir = indir + '/' if indir[-1] != '/' else indir
+	injson = indir + 'result.json'
+	incas = indir + 'rawCas.fna'
+	with open(injson, 'r') as fin:
+		result = json.loads(fin.read())
+
+	isolate_id = indir.split('/')[-2].split('_')[1]
+
+	if isolate_id not in results.keys():
+		results[isolate_id] = {}
+		array_num = 0
+	for i in result['Sequences']:
+		if len(i['Crisprs']) > 0:
+			for j in i['Crisprs']:
+				if int(j['Evidence_Level']) > 1:
+					array_num += 1
+					results[isolate_id][array_num] = {
+						'Contig' : "_".join(j["Name"].split("_")[:-1]), 
+						'Location' : str(str(j["Start"]) + " : " + str(j["End"])), 
+						'Number_Spacers' : j["Spacers"], 
+						'DR_Consensus' : j["DR_Consensus"], 
+						'DR_Type' : get_repeat_info(CRISPR_types_dict, j["DR_Consensus"])[0],
+						'DR_Hamming_Dist' : get_repeat_info(CRISPR_types_dict, j["DR_Consensus"])[1],
+						"Array_encoded" : '',
+						'Spacer' : [], 
+						'Spacer_encoded' : [], 
+						'DR' : [],
+						'Leader' : ''
+						
+						}
+					if j['Potential_Orientation'] == '+':
+						for k in j['Regions']:
+							if 'FLANK' in k['Type']:
+								if k['Leader'] == 1:
+									results[isolate_id][array_num]['Leader'] = k['Sequence']
+							else:	
+								results[isolate_id][array_num][k['Type']].append(k['Sequence']) # Adds the sequence to either the DR or spacer list for that array depending on the 'Type' in the json file.
+								if k['Type'] == 'Spacer':
+									if k['Sequence'] in spacers.keys():
+										results[isolate_id][array_num]['Spacer_encoded'].append(str(spacers[k['Sequence']]))
+									else:
+										spacers[k['Sequence']] = nspacers_seen
+										results[isolate_id][array_num]['Spacer_encoded'].append(str(nspacers_seen))
+										nspacers_seen += 1
+
+					else:
+						results[isolate_id][array_num]['DR_Consensus'] = rev_comp.rev_comp(j["DR_Consensus"])
+						for k in reversed(j['Regions']):
+							if 'FLANK' in k['Type']:
+								if k['Leader'] == 1:
+									results[isolate_id][array_num]['Leader'] = rev_comp.rev_comp(k['Sequence'])
+							else:	
+								results[isolate_id][array_num][k['Type']].append(rev_comp.rev_comp(k['Sequence']))
+								if k['Type'] == 'Spacer':
+									if rev_comp.rev_comp(k['Sequence']) in spacers.keys():
+										results[isolate_id][array_num]['Spacer_encoded'].append(str(spacers[rev_comp.rev_comp(k['Sequence'])]))
+									else:
+										spacers[rev_comp.rev_comp(k['Sequence'])] = nspacers_seen
+										results[isolate_id][array_num]['Spacer_encoded'].append(str(nspacers_seen))
+										nspacers_seen += 1
+					# array stuff
+					if " ".join(results[isolate_id][array_num]['Spacer']) in arrays.keys():
+						results[isolate_id][array_num]["Array_encoded"] = arrays[" ".join(results[isolate_id][array_num]['Spacer'])]
+					else:
+						arrays[" ".join(results[isolate_id][array_num]['Spacer'])] = str(narrays_seen)
+						arrays_encoded[" ".join(results[isolate_id][array_num]['Spacer_encoded'])] = str(narrays_seen)
+						results[isolate_id][array_num]["Array_encoded"] = str(narrays_seen)
+						narrays_seen += 1
+	Cas_results[isolate_id] = {
+		'Cas_found' : "FALSE",
+		'Cas_types' : [],
+		'Cas_completeness' : {},
+		'Cas_completeness_binary' : '1', # Will be set to 0 if any identified CRISPR subtypes don't have all mandatory genes.
+		'Cas_list' : {}
+		}
+	Cas_headers = fasta_to_dict(incas).keys()
+	if len(Cas_headers) > 0:
+		Cas_results[isolate_id]['Cas_found'] = "TRUE"
+		for header in Cas_headers:
+			elements = header.split('|')
+			CR_type = elements[2]
+			gene = elements[3].split()[0]
+			if CR_type not in Cas_results[isolate_id]['Cas_types']:
+				Cas_results[isolate_id]['Cas_types'].append(CR_type)
+				if CR_type != "CAS":
+					Cas_results[isolate_id]['Cas_completeness'][CR_type] = [0, Cas_dict[CR_type]['n_mandatory']]
+					if Cas_dict[CR_type][gene] == 'mandatory':
+						Cas_results[isolate_id]['Cas_completeness'][CR_type][0] += 1
+
+				Cas_results[isolate_id]['Cas_list'][CR_type] = [gene]
+			else:
+				if CR_type != "CAS":
+					if gene not in Cas_results[isolate_id]['Cas_list'][CR_type] and Cas_dict[CR_type][gene] == 'mandatory':
+						Cas_results[isolate_id]['Cas_completeness'][CR_type][0] += 1
+				Cas_results[isolate_id]['Cas_list'][CR_type].append(gene)
+		for v in Cas_results[isolate_id]['Cas_completeness'].values():
+			if v[0] < v[1]:
+				Cas_results[isolate_id]['Cas_completeness_binary'] = '0'
+
+for k,v in Cas_results.items():
+	print(k,v)
+
+		
+
+		
+
+
+
+
+CRISPR_sum_to_write = [["Strain,Has_CRISPR,Array_count,Spacers,Spacers_encoded,Array_IDs,Array_locations,Array_sizes,Repeat_sequences,Array_CRISPR_types,Repeat_scores, Leader, Cas_found, Cas_types, Cas_completeness, Cas_list"]]
+
+for k,v in results.items():
+	if len(v) == 0:
+		CRISPR_sum_to_write.append([k, 'FALSE'])
+	else:
+		Strain = k
+		Has_CRISPR = "TRUE"
+		Array_count = str(len(v))
+		Spacers = []
+		Spacers_encoded = []
+		Array_ids = []
+		Array_locations = []
+		Array_sizes = []
+		Repeat_sequences = []
+		Array_types = []
+		Repeat_scores = []
+		Leaders = []
+		for array_num, feature_dict in v.items():
+			Spacers.append(str(array_num) + ": " + " ".join(feature_dict['Spacer']))
+			Spacers_encoded.append(str(array_num) + ": " + " ".join(feature_dict['Spacer_encoded']))
+			Array_ids.append("'" + str(array_num) + ": " + str(feature_dict['Array_encoded']) + "'")
+			Array_locations.append(str(array_num) + ": " + " ".join(feature_dict['Location']))
+			Array_sizes.append("'" + str(array_num) + ": " + str(feature_dict['Number_Spacers']) +"'")
+			Repeat_sequences.append(str(array_num) + ": " + feature_dict['DR_Consensus'])
+			Array_types.append(str(array_num) + ": " + feature_dict['DR_Type'])
+			Repeat_scores.append("'" + str(array_num) + ": " + str(feature_dict['DR_Hamming_Dist']) + "'")
+			Leaders.append(str(array_num) + ": " + feature_dict['Leader'])
+		Spacers = "\t".join(Spacers)
+		Spacers_encoded = "\t".join(Spacers_encoded)
+		Array_ids = "\t".join(Array_ids)
+		Array_locations = "\t".join(Array_locations)
+		Array_sizes = "\t".join(Array_sizes)
+		Repeat_sequences = "\t".join(Repeat_sequences)
+		Array_types = "\t".join(Array_types)
+		Repeat_scores = "\t".join(Repeat_scores)
+		Leaders = "\t".join(Leaders)
+		Cas_found = Cas_results[isolate_id]['Cas_found']
+		Cas_types = " ".join(Cas_results[isolate_id]['Cas_types'])
+		Cas_completeness = []
+		Cas_list = []
+		for crtype,feature in Cas_results[isolate_id]['Cas_completeness'].items():
+			Cas_completeness.append(crtype + ': ' + str(feature[0]) + '/' + str(feature[1])) 
+			Cas_list.append(crtype + ': ' + " ".join(Cas_results[isolate_id]['Cas_list'][crtype]))
+		Cas_completeness = '\t'.join(Cas_completeness)
+		Cas_completeness_binary = Cas_results[isolate_id]['Cas_completeness_binary']
+		Cas_list = '\t'.join(Cas_list)
+		CRISPR_sum_to_write.append([Strain,Has_CRISPR,Array_count,Spacers,Spacers_encoded,Array_ids, Array_locations,Array_sizes,Repeat_sequences,Array_types,Repeat_scores, Leaders, Cas_found, Cas_types, Cas_completeness, Cas_completeness_binary, Cas_list])
+
+
+# with open(outdir + "CRISPR_summary_table.csv", 'w+') as fout:
+# 	fout.write("\n".join([','.join(line) for line in CRISPR_sum_to_write]))
+
---
+++ Find_CRISPR_arrays/reps2spacers.py
@@ -0,0 +1,350 @@
+#!/usr/bin/env python3
+
+
+# AUTHOR        :    ALAN COLLINS
+# VERSION       :    v1.3.1
+# DATE          :    2021-4-25
+# DESCRIPTION   :    Given fasta CRISPR repeats and a blast db of genomes, pulls out spacer arrays of >= 2 spacers
+# CHANGELOG
+# V1.1 
+#   Added contig information to array location column in .csv output file. 
+#   Contig information is taken from the sseqid of the blast result.
+# V1.1
+#   Removed print commands that I had forgotten to get rid of.
+# v1.2
+#   Fixed mistake where the wrong array_class attribute was being used for storing array ends points for 3'-5' arrays.
+#   added trailing newlines to the end of output files.
+# v1.3
+#   Fixed issue where the end of an array was not found properly if there was only one array in one genome.
+#   Removed btop from blast as it was unused
+#   Added stderr catching and error printing for blastn and blastdbcmd run in case it fails.
+# v1.3.1
+#   Fixed issue handling large blastdbs that are split over multiple .nhr files.
+
+import sys
+import os
+import argparse
+import textwrap as _textwrap
+import subprocess
+import re
+from collections import defaultdict
+
+class LineWrapRawTextHelpFormatter(argparse.RawDescriptionHelpFormatter):
+    """
+    Short function for argparse that wraps text properly when printing to terminal
+    """
+    def _split_lines(self, text, width):
+        text = self._whitespace_matcher.sub(' ', text).strip()
+        return _textwrap.wrap(text, width)
+
+class blast_result():
+    def __init__(self, blast_line):
+        bits = blast_line.split('\t')
+        self.qseqid = bits[0]
+
+        self.sseqid = bits[1]
+        self.pident = float(bits[2])
+        self.length = int(bits[3])
+        self.mismatch = int(bits[4])
+        self.gapopen = bits[5]
+        self.qstart = int(bits[6])
+        self.qend = int(bits[7])
+        self.sstart = int(bits[8])
+        self.send = int(bits[9])
+        self.evalue = bits[10]
+        self.bitscore = bits[11]
+        self.qlen = int(bits[12])
+        self.slen = bits[13]
+        self.strand = 'plus' if self.sstart < self.send else 'minus'
+        self.trunc = False # Store whether repeat might be truncated (i.e. blast result wasn't full qlen)
+        self.sstart_mod = False # If repeat appears tuncated, store revised genome locations here
+        self.send_mod = False
+
+class blast_entry():
+    def __init__(self, blast_result):
+        self.qseqid = blast_result.qseqid
+        self.sseqid = blast_result.sseqid
+        if blast_result.sstart < blast_result.send:
+            self.strand = 'plus'
+            if blast_result.trunc:
+                self.sstart = blast_result.sstart_mod
+                self.send = blast_result.send_mod
+            else:
+                self.sstart = blast_result.sstart
+                self.send = blast_result.send
+        else:
+            self.strand = 'minus'
+            if blast_result.trunc:
+                self.sstart = blast_result.send_mod
+                self.send = blast_result.sstart_mod
+            else:
+                self.sstart = blast_result.send
+                self.send = blast_result.sstart
+
+class array_class():
+    def __init__(self):
+        self.genome = ''
+        self.contig = ''
+        self.start = 0
+        self.stop = 0
+        self.repeats = []
+        self.repeat_id = ''
+        self.spacers = []
+        self.spacer_ids = []
+        self.array_id = 0
+
+def hamming(string1, string2):
+    dist = 0
+    for i in range(max(len(string1), len(string2))):
+        if i < len(string1) and i < len(string2):
+            if string1[i] != string2[i]:
+                dist += 1
+        else:
+            dist += 1
+
+    return dist
+
+def rev_comp(string):
+    rev_str = ''
+    rev_comp_lookup = {"A" : "T", "T" : "A", "C" : "G", "G" : "C", "a" : "t", "t" : "a", "c" : "g", "g" : "c"}
+    for i in reversed(string):
+        if i in "ATCGatcg":
+            rev_str += rev_comp_lookup[i]
+        else:
+            rev_str += i
+    return rev_str
+
+
+def run_blastcmd(db, seqid, start, stop, strand):
+    x = subprocess.run("blastdbcmd -db {} -entry {} -range {}-{} -strand {}".format(db, seqid, start, stop, strand), shell=True, universal_newlines = True, capture_output=True)
+    if x.stderr:
+        print("ERROR running blastdbcmd on {}:\n{}".format(db, x.stderr))
+        sys.exit()
+    else:
+        return x.stdout.split('\n')[1]
+
+
+def blastn_to_arrays(query, db, pattern):
+    
+    blastn_command = "blastn -query {} -db {} -task blastn-short -outfmt '6 std qlen slen'".format(query, db)
+    blast_run = subprocess.run(blastn_command, shell=True, universal_newlines = True, capture_output=True)
+    # blast_lines = [blast_result(i) for i in subprocess.run(blastn_command, shell=True, universal_newlines = True, capture_output=True).stdout.split('\n') if len(i) > 0]
+    if blast_run.stderr:
+        print("ERROR running blast on {}:\n{}".format(db, blast_run.stderr))
+        sys.exit()
+    blast_lines = [blast_result(i) for i in blast_run.stdout.split('\n') if len(i) > 0]
+    
+
+    good_hits = []
+
+    for line in blast_lines:
+        if line.length > 0.9 * line.qlen: # Keep any blast hits where the match length is over 90% of query
+            if line.pident > 90:
+                if line.length != line.qlen: # If match length isn't 100%, extend repeat to expected length
+                    line.trunc = True
+                    if line.sstart < line.send: # if the blast hit is on the positive strand
+                        if line.qstart != 1: # if the 5' end of repeat is missing
+                            line.sstart_mod = line.sstart - (line.qstart - 1) # adjust sstart by the number of bases missing from repeat 5' end
+                        if line.qend != line.length: # if the 3' end of the repeat is missing
+                            line.send_mod = line.send + (line.qend - line.length) # adjust send by the number of bases missing from repeat 3' end
+                    elif line.sstart > line.send: # if the blast hit is on the negative strand
+                        if line.qstart != 1: # if the 5' end of repeat is missing
+                            line.send_mod = line.send - (line.qstart - 1) # adjust sstart by the number of bases missing from repeat 5' end
+                        if line.qend != line.length: # if the 3' end of the repeat is missing
+                            line.sstart_mod = line.sstart + (line.qend - line.length) # adjust send by the number of bases missing from repeat 3' end
+                good_hits.append(blast_entry(line))
+    good_hits_sorted = sorted(good_hits, key = lambda x: (x.sseqid, x.strand, x.sstart))
+    array_entries = identify_same_array_hits(good_hits_sorted)
+    # print(array_entries)
+    arrays = []
+
+    if len(array_entries) > 0:
+        for a in array_entries: # a[0] is the first repeat in the array. a[-1] is the last
+            array = array_class()
+            array.genome = re.match(pattern, a[0].sseqid)[0]
+            n_reps = len(a)
+            array.repeat_id = a[0].qseqid
+            array.contig = a[0].sseqid
+            if a[0].strand == 'plus':
+                array.start = a[-1].send
+                array.stop = a[0].sstart
+                for i, entry in enumerate(a):
+
+                    rep = run_blastcmd(db, entry.sseqid, entry.sstart, entry.send, entry.strand)
+                    array.repeats.append(rep)
+                    if i+1 != n_reps:
+                        spacer = run_blastcmd(db, entry.sseqid, entry.send+1, a[i+1].sstart-1, entry.strand)
+                        array.spacers.append(spacer)
+            
+            else:
+                array.start = a[0].send
+                array.stop = a[-1].sstart
+                for i, entry in enumerate(reversed(a)):
+                    rep = run_blastcmd(db, entry.sseqid, entry.sstart, entry.send, entry.strand)
+                    array.repeats.append(rep)
+                    if i+1 != n_reps:
+                        spacer = run_blastcmd(db, entry.sseqid, a[-(i+2)].send+1, entry.sstart-1, entry.strand)
+                        array.spacers.append(spacer)
+
+            arrays.append(array)
+
+    return arrays
+
+
+def identify_same_array_hits(blast_entries): 
+    # Using position and orientation of sorted (by contig and start position) repeats, 
+    # group them into arrays if there are at least 3 repeats in a group. 
+    # Otherwise discard them
+    arrays = []
+    n_spacers = 0 # count spacers in the current array
+    last_contig = False
+    last_loc = False # store start coord of last repeat
+    last_strand = False # store orientation of last repeat
+    for entry in blast_entries:
+        if last_loc:
+            if last_loc > entry.sstart - 150 and last_strand == entry.strand and last_contig == entry.sseqid:
+                this_array.append(entry)
+                last_loc = entry.sstart
+                if entry == blast_entries[-1]:
+                    if len(this_array) > 2:
+                        arrays.append(this_array)
+                        last_loc = entry.sstart
+                        last_strand = entry.strand
+                        last_contig = entry.sseqid
+                        this_array = [entry]
+
+                    else:
+                        last_loc = entry.sstart
+                        last_strand = entry.strand
+                        last_contig = entry.sseqid
+                        this_array = [entry]
+                
+            else:
+                if len(this_array) > 2:
+                    arrays.append(this_array)
+                    last_loc = entry.sstart
+                    last_strand = entry.strand
+                    last_contig = entry.sseqid
+                    this_array = [entry]
+
+                else:
+                    last_loc = entry.sstart
+                    last_strand = entry.strand
+                    last_contig = entry.sseqid
+                    this_array = [entry]
+        else:
+            last_loc = entry.sstart
+            last_strand = entry.strand
+            last_contig = entry.sseqid
+            this_array = [entry]
+
+    return arrays
+
+parser = argparse.ArgumentParser(
+    description="Given fasta CRISPR repeats and a blast db of genomes, pulls out spacer arrays of >= 2 spacers",
+    formatter_class=LineWrapRawTextHelpFormatter)
+parser.add_argument(
+    "-r", dest="repeats_file", required = True,
+    help=""
+    )
+parser.add_argument(
+    "-d", dest="blast_db_path", required = True,
+    help="path to blast db files (not including the file extensions)"
+    )
+parser.add_argument(
+    "-o", dest="outdir", required = True,
+    help="path to directory you want output files saved in"
+    )
+parser.add_argument(
+    "-p", dest="regex_pattern", required = True,
+    help="In order to identify which genome a spacer was found in, put the genome id in your genome files fasta headers before making the blast db and then provide a regex pattern that can extract that id from the fasta header here. e.g. for the fasta header: >animaloris_GCA_900637855.1_LR134440.1 the genome id (animaloris_GCA_900637855.1) can be extracted with [A-Za-z]+_GCA_[0-9]+\.[0-9]"
+    )
+
+
+args = parser.parse_args(sys.argv[1:])
+
+outdir = args.outdir + '/' if args.outdir[-1] != '/' else args.outdir
+
+# Find the names of all the genomes being searched by looking for the user-provided regex in the blast-db .nhr files.
+# for all genomes, start their entry in the genome_CRISPR_dict with the placeholder False. This indicated no CRISPRs found.
+# If a CRISPR array is found later this entry will be overwritten with infor about the arrays.
+genome_CRISPR_dict = { k : ['False'] for k in subprocess.run("grep -haoE '{}' {}*.nhr".format(args.regex_pattern, args.blast_db_path), shell=True, universal_newlines = True, capture_output=True).stdout.split('\n') if len(k) > 0} 
+
+
+all_arrays = []
+
+all_arrays += blastn_to_arrays(args.repeats_file, args.blast_db_path, args.regex_pattern)
+
+
+spacer_dict = {}
+spacer_count_dict = defaultdict(int)
+array_dict = {}
+array_count = 0
+
+for i, array in enumerate(all_arrays):
+    for spacer in array.spacers:
+        if spacer in spacer_dict.keys():
+            all_arrays[i].spacer_ids.append(spacer_dict[spacer])
+        else:
+            spacer_count_dict[array.repeat_id] +=1
+            spacer_dict[spacer] = 'rep_' + array.repeat_id + '_sp_' + str(spacer_count_dict[array.repeat_id])
+            all_arrays[i].spacer_ids.append(spacer_dict[spacer])
+            
+    if tuple(array.spacer_ids) in array_dict.keys():
+        all_arrays[i].array_id = array_dict[tuple(array.spacer_ids)]
+    else:
+        array_count += 1
+        all_arrays[i].array_id = array_count
+        array_dict[tuple(array.spacer_ids)] = array_count
+
+genome_arrays = defaultdict(list) # list of arrays present in each genome
+array_genomes = defaultdict(list) # list of genomes with this array
+spacer_genomes = defaultdict(list) # list of genomes with this spacers
+
+for array in all_arrays:
+    genome_arrays[array.genome].append(array)
+    array_genomes[array.array_id].append(array.genome)
+    for spacer in array.spacer_ids:
+        spacer_genomes[spacer].append(array.genome)
+
+
+for genome, arrays in genome_arrays.items():
+    n_arrays = str(len(arrays))
+    spacers_list = "\t".join(["{}: {}".format(i+1, " ".join(array.spacers)) for i, array in enumerate(arrays)])
+    spacer_id_list = "\t".join(["{}: {}".format(i+1, " ".join(array.spacer_ids)) for i, array in enumerate(arrays)])
+    array_id_list = "\t".join(["{}: {}".format(i+1, array.array_id) for i, array in enumerate(arrays)])
+    array_locs = "\t".join(["{}: {} {} {}".format(i+1, array.contig, array.start, array.stop) for i, array in enumerate(arrays)])
+    genome_CRISPR_dict[genome] = ['True', n_arrays, spacers_list, spacer_id_list, array_id_list, array_locs]
+
+outcontents = ["Genome,Has_CRISPR,Array_count,Spacers,Spacer_IDs,Array_IDs,Array_locations"]
+
+for k,v in genome_CRISPR_dict.items():
+    outcontents.append(",".join([k]+v))
+
+with open(outdir + "CRISPR_summary_table.csv", 'w') as fout:
+    fout.write('\n'.join(outcontents)+ '\n')
+
+outcontents = ["Array_ID\tgenomes_with_array"]
+
+for k,v in array_genomes.items():
+    outcontents.append('{}\t{}'.format(k, ' '.join(v)))
+
+with open(outdir + "Array_representatives.txt", 'w') as fout:
+    fout.write('\n'.join(outcontents) + '\n')
+
+outcontents = []
+
+for k,v in spacer_dict.items():
+    outcontents.append(">{}\n{}".format(v,k))
+
+with open(outdir + "CRISPR_spacers.fna", 'w') as fout:
+    fout.write('\n'.join(outcontents) + '\n')
+
+outcontents = ["Spacer_ID\tGenomes_with_spacer"]
+
+for k,v in spacer_genomes.items():
+    v = list(set(v))
+    outcontents.append("{}\t{}".format(k, " ".join(v)))
+
+with open(outdir + "Spacer_representatives.txt", 'w') as fout:
+    fout.write('\n'.join(outcontents) + '\n')
