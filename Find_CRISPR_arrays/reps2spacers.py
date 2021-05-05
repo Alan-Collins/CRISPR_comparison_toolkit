6b8e18233650f4148a87590f6a8bea73c2849ee0
Alan Collins <alan.collins@bath.edu>
2021-05-05 11:46:06 -0500

    Added multiprocess attempt and argpars options
    Added option to control regex type. Wrote function to use multiprocess to lookup spacer sequences for each array

--- Find_CRISPR_arrays/reps2spacers.py
+++ Find_CRISPR_arrays/reps2spacers.py
@@ -2,7 +2,7 @@
 
 
 # AUTHOR        :    ALAN COLLINS
-# VERSION       :    v1.4
+# VERSION       :    v2
 # DATE          :    2021-5-5
 # DESCRIPTION   :    Given fasta CRISPR repeats and a blast db of genomes, pulls out spacer arrays of >= 2 spacers
 # CHANGELOG
@@ -20,15 +20,17 @@
 #   Added stderr catching and error printing for blastn and blastdbcmd run in case it fails.
 # v1.3.1
 #   Fixed issue handling large blastdbs that are split over multiple .nhr files.
-# v1.4
+# v2
 #   Added e-value cutoff and max_target_seqs options to the blastn step so that blast doesn't stop looking for repeats prematurely with large databases.
 #   Added argparse options to control thread count, evalue, and max_target_seqs blast options
-
+#   Added multithreading to the spacer finding function to the blastdbcmd step that finds spacer sequences as this is the slowest part.
+#   Added user provided option to control type of regex used for grep against database files.
 
 import sys
 import os
 import argparse
 import subprocess
+import multiprocessing
 import re
 from collections import defaultdict
 
@@ -59,9 +61,11 @@ class blast_result():
         self.send_mod = False
 
 class blast_entry():
-    def __init__(self, blast_result):
+    def __init__(self, blast_result, args):
         self.qseqid = blast_result.qseqid
         self.sseqid = blast_result.sseqid
+        self.regex_pattern = args.regex_pattern
+        self.blastdb = args.blast_db_path
         if blast_result.sstart < blast_result.send:
             self.strand = 'plus'
             if blast_result.trunc:
@@ -122,6 +126,45 @@ def run_blastcmd(db, seqid, start, stop, strand):
         return x.stdout.split('\n')[1]
 
 
+def pool_MP_spacer_finder(array_entries, threads, chunksize):
+
+    pool = multiprocessing.Pool(processes=int(threads))
+
+    pool.imap_unordered(build_arrays_MP, array_entries, chunksize)
+    pool.close()
+    pool.join()
+
+
+def build_arrays_MP(array_entry):
+    array = array_class()
+    array.genome = re.match(array_entry[0].regex_pattern, array_entry[0].sseqid)[0]
+    n_reps = len(array_entry)
+    array.repeat_id = array_entry[0].qseqid
+    array.contig = array_entry[0].sseqid
+    if array_entry[0].strand == 'plus':
+        array.start = array_entry[-1].send
+        array.stop = array_entry[0].sstart
+        for i, entry in enumerate(array_entry):
+
+            rep = run_blastcmd(array_entry[0].blast_db_path, entry.sseqid, entry.sstart, entry.send, entry.strand)
+            array.repeats.append(rep)
+            if i+1 != n_reps:
+                spacer = run_blastcmd(array_entry[0].blast_db_path, entry.sseqid, entry.send+1, array_entry[i+1].sstart-1, entry.strand)
+                array.spacers.append(spacer)
+    
+    else:
+        array.start = array_entry[0].send
+        array.stop = array_entry[-1].sstart
+        for i, entry in enumerate(reversed(array_entry)):
+            rep = run_blastcmd(args.blast_db_path, entry.sseqid, entry.sstart, entry.send, entry.strand)
+            array.repeats.append(rep)
+            if i+1 != n_reps:
+                spacer = run_blastcmd(args.blast_db_path, entry.sseqid, array_entry[-(i+2)].send+1, entry.sstart-1, entry.strand)
+                array.spacers.append(spacer)
+    return array
+
+
+
 def blastn_to_arrays(args):
     
     blastn_command = "blastn -query {} -db {} -task blastn-short -outfmt '6 std qlen slen' -num_threads {} -max_target_seqs {} -evalue {} {}".format(args.repeats_file, args.blast_db_path, args.numthreads, args.max_target_seqs, args.evalue, args.other_blast_options)
@@ -150,46 +193,20 @@ def blastn_to_arrays(args):
                             line.send_mod = line.send - (line.qstart - 1) # adjust sstart by the number of bases missing from repeat 5' end
                         if line.qend != line.length: # if the 3' end of the repeat is missing
                             line.sstart_mod = line.sstart + (line.qend - line.length) # adjust send by the number of bases missing from repeat 3' end
-                good_hits.append(blast_entry(line))
+                good_hits.append(blast_entry(line, args))
     good_hits_sorted = sorted(good_hits, key = lambda x: (x.sseqid, x.strand, x.sstart))
-    array_entries = identify_same_array_hits(good_hits_sorted)
+    array_entries = identify_same_array_hits(good_hits_sorted, args)
     # print(array_entries)
+    
     arrays = []
 
     if len(array_entries) > 0:
-        for a in array_entries: # a[0] is the first repeat in the array. a[-1] is the last
-            array = array_class()
-            array.genome = re.match(args.regex_pattern, a[0].sseqid)[0]
-            n_reps = len(a)
-            array.repeat_id = a[0].qseqid
-            array.contig = a[0].sseqid
-            if a[0].strand == 'plus':
-                array.start = a[-1].send
-                array.stop = a[0].sstart
-                for i, entry in enumerate(a):
-
-                    rep = run_blastcmd(args.blast_db_path, entry.sseqid, entry.sstart, entry.send, entry.strand)
-                    array.repeats.append(rep)
-                    if i+1 != n_reps:
-                        spacer = run_blastcmd(args.blast_db_path, entry.sseqid, entry.send+1, a[i+1].sstart-1, entry.strand)
-                        array.spacers.append(spacer)
-            
-            else:
-                array.start = a[0].send
-                array.stop = a[-1].sstart
-                for i, entry in enumerate(reversed(a)):
-                    rep = run_blastcmd(args.blast_db_path, entry.sseqid, entry.sstart, entry.send, entry.strand)
-                    array.repeats.append(rep)
-                    if i+1 != n_reps:
-                        spacer = run_blastcmd(args.blast_db_path, entry.sseqid, a[-(i+2)].send+1, entry.sstart-1, entry.strand)
-                        array.spacers.append(spacer)
-
-            arrays.append(array)
+        arrays.append(pool_MP_spacer_finder(array_entries, args.num_threads, int(len(array_entries)//args.num_threads)))
 
     return arrays
 
 
-def identify_same_array_hits(blast_entries): 
+def identify_same_array_hits(blast_entries, args): 
     # Using position and orientation of sorted (by contig and start position) repeats, 
     # group them into arrays if there are at least 3 repeats in a group. 
     # Otherwise discard them
@@ -253,7 +270,7 @@ parser.add_argument(
     help="path to directory you want output files saved in"
     )
 parser.add_argument(
-    "-e", dest="evalue", required = False, default='1e-5',
+    "-e", dest="evalue", required = False, default='1',
     help="set the evalue cutoff below which blastn will keep blast hits when looking for CRISPR repeats in your blast database. Useful for reducing inclusion of low quality blast hits with big databases in combination with the -m option."
     )
 parser.add_argument(
@@ -261,7 +278,7 @@ parser.add_argument(
     help="Set the max_target_seqs option for blastn when looking for CRISPR repeats in your blast database. Blast stops looking for hits after finding and internal limit (N_i) sequences for each query sequence, where N_i=2*N+50. These are just the first N_i sequences with better evalue scores than the cutoff, not the best N_i hits. Because of the nature of the blast used here (small number of queries with many expected hits) it may be necessary to increase the max_target_seqs value to avoid blast ceasing to search for repeats before all have been found. The blast default value is 500. The default used here is 10,000. You may want to reduce it to increase speed or increase it to make sure every repeat is being found. If increasing this value (e.g. doubling it) finds no new spacers then you can be confident that this is not an issue with your dataset."
     )
 parser.add_argument(
-    "-t", dest="num_threads", required = False, default='1'
+    "-t", dest="num_threads", required = False, default='1',
     help="Number of threads you want to use for the blastn step of this script."
     )
 parser.add_argument(
@@ -269,7 +286,11 @@ parser.add_argument(
     help="In order to identify which genome a spacer was found in, put the genome id in your genome files fasta headers before making the blast db and then provide a regex pattern that can extract that id from the fasta header here. e.g. for the fasta header: >animaloris_GCA_900637855.1_LR134440.1 the genome id (animaloris_GCA_900637855.1) can be extracted with [A-Za-z]+_GCA_[0-9]+\.[0-9]"
     )
 parser.add_argument(
-    "-x", dest="other_blast_options", required = False, default=''
+    "-q", dest="regex_type", required = True, default='P',
+    help="Regex type declaration option for grep (e.g. E, P, etc). Your regex pattern will be used with grep to find genome names in the .nhr file of your blast database. If you want to use regex patterns like \d+ you need to use the P option for Perl regex. Test your regex using the function 'grep -haoP' or 'grep -haoE' etc with your regex pattern against the nhr file in your blastdb."
+    )
+parser.add_argument(
+    "-x", dest="other_blast_options", required = False, default='',
     help="If you want to include any other options to control the blastn command, you can add them here. Options you should not provide here are: blastn -query -db -task -outfmt -num_threads -max_target_seqs -evalue"
     )
 
@@ -281,7 +302,7 @@ outdir = args.outdir + '/' if args.outdir[-1] != '/' else args.outdir
 # Find the names of all the genomes being searched by looking for the user-provided regex in the blast-db .nhr files.
 # for all genomes, start their entry in the genome_CRISPR_dict with the placeholder False. This indicated no CRISPRs found.
 # If a CRISPR array is found later this entry will be overwritten with infor about the arrays.
-genome_CRISPR_dict = { k : ['False'] for k in subprocess.run("grep -haoE '{}' {}*.nhr".format(args.regex_pattern, args.blast_db_path), shell=True, universal_newlines = True, capture_output=True).stdout.split('\n') if len(k) > 0} 
+genome_CRISPR_dict = { k : ['False'] for k in subprocess.run("grep -hao{} '{}' {}*.nhr".format(args.regex_type, args.regex_pattern, args.blast_db_path), shell=True, universal_newlines = True, capture_output=True).stdout.split('\n') if len(k) > 0} 
 
 
 all_arrays = []
