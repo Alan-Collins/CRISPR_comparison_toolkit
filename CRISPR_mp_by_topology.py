0cfecabc3a25dca0e6a4b91e281c6153828b55f6
Alan Collins <alan.collins@bath.edu>
2021-08-02 17:33:03 -0500

    Renamed random option for make_topologies to randomize to remove conflict with random package. Started building implementation of construction of starting trees.

--- CRISPR_mp_by_topology.py
+++ CRISPR_mp_by_topology.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
-# AUTHOR      :  ALAN COLLINS
-# VERSION     :  v1
-# DATE        :  2021-8-2
+# AUTHOR	  :  ALAN COLLINS
+# VERSION	 :  v1
+# DATE		:  2021-8-2
 # DESCRIPTION :  Generate random bifircating tree topologies and calculate parsimony of each then return the best.
 
 import sys
 import argparse
 import dendropy
 import CRISPR_mp
-from random import randint
+import random
 
 
-def make_topologies(leaves, root=False):
+def make_topologies(leaves, root=False, randomize=False):
 	"""
 	Given a number of leaves for a tree, generate all possible configurations of the rooted bifurcating tree.
 	Args:
 		num_leaves (list): Leaves to be placed in the tree.
 		root (int): The 0 base integer location at which to place the root and generate all tree topologies with that root.
+		randomize (bool): Would you like a single random tree to be returned?
 	Yields:
 		 Newick string of tree or subtree.
 	"""
 	if len(leaves) == 1: # When all nodes have been joined, return the list in a tidied format.
 		yield leaves[0]
 	else:
-		if root:
+		if randomize:
+			root = random.randint(1,len(leaves)-1) # Root the tree at a random point
+			for left in make_topologies(leaves[:root], random.randint(1, root), True): # Root the subtree to the left at a random point
+				for right in make_topologies(leaves[root:], random.randint(root, len(leaves)-1), True): # Root the subtree to the right at a random point
+					yield "({},{})".format(left, right)
+		elif root:
 			for left in make_topologies(leaves[:root]): # Make subtrees to the left of (sub)tree root
 				for right in make_topologies(leaves[root:]): # Make subtrees to the right of (sub)tree root
 					yield "({},{})".format(left, right)
@@ -33,7 +39,6 @@ def make_topologies(leaves, root=False):
 				for left in make_topologies(leaves[:i]): # Make subtrees to the left of (sub)tree root
 					for right in make_topologies(leaves[i:]): # Make subtrees to the right of (sub)tree root
 						yield "({},{})".format(left, right)
-
 
 
 def main():
@@ -55,8 +60,15 @@ def main():
 
 	args = parser.parse_args(sys.argv[1:])
 
+	array_choices = [random.sample(args.arrays_to_join, len(args.arrays_to_join)) for i in range(args.replicates)]
+	
+	starting_trees = []
+	for order in array_choices:
+		t = next(make_topologies(order, randomize=True)) + ";"
+		starting_trees.append(dendropy.Tree.get(data=t, schema="newick"))
 
-	print([t for t in make_topologies(args.arrays_to_join, 2)])
+	for tree in starting_trees:
+		print(tree.as_ascii_plot())
 
 
 
