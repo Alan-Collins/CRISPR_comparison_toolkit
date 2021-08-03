b4368dcda917777beb701663879be171f2693cb4
Alan Collins <alan.collins@bath.edu>
2021-08-03 12:56:56 -0500

    Added command line control of event costs and implemented initial population of the tree with array details and edge length.

--- CRISPR_mp_by_topology.py
+++ CRISPR_mp_by_topology.py
@@ -58,11 +58,39 @@ def main():
 			help="Specify number of replicates of tree building to perform. The more replicates, the greater the chance that a better tree will be found. Default: 1"
 		)
 	parser.add_argument(
+		"-q",  dest="acquisition", type=int, nargs="?", default = 1,
+			help="Specify the parsimony cost of a spacer acquisition event. Default: 1"
+		)
+	parser.add_argument(
+		"-i",  dest="indel", type=int, nargs="?", default = 10,
+			help="Specify the parsimony cost of an indel event involving one or more spacers. Default: 1"
+		)
+	parser.add_argument(
+		"-z",  dest="rep_indel", type=int, nargs="?", default = 50,
+			help="Specify the parsimony cost of an indel event involving one or more spacers that is independently acquired in multiple arrays. Default: 50"
+		)
+	parser.add_argument(
+		"-d",  dest="duplication", type=int, nargs="?", default = 1,
+			help="Specify the parsimony cost of a duplication event involving one or more spacers. Default: 1"
+		)
+	parser.add_argument(
+		"-l",  dest="trailer_loss", type=int, nargs="?", default = 1,
+			help="Specify the parsimony cost of the loss of a spacer from the trailer end of the array. Default: 1"
+		)
+	parser.add_argument(
 		"arrays_to_join", nargs="*",  
 		help="Specify the IDs of the arrays you want to join. If none provided, joins all arrays in the provided array representatives file. **If given, must come at the end of your command after all other arguments.**"
 		)
 
 	args = parser.parse_args(sys.argv[1:])
+
+	event_costs = { 
+					"acquisition" : args.acquisition,
+					"indel" : args.indel,
+					"repeated_indel" : args.rep_indel,
+					"duplication": args.duplication,
+					"trailer_loss": args.trailer_loss
+					}
 
 	array_spacers_dict = {}
 	with open(args.array_file, 'r') as fin:
@@ -88,16 +116,48 @@ def main():
 			trees.append(next(make_topologies(array_orders[i], randomize=True))+";")
 
 	best_score = 9999999999
+	for t in trees[:3]:
+		# Control settings
+		Incomplete_tree = False
+		node_count = 0
 
-	for t in trees[:1]:
 		# Initialize tree as dendropy Tree instance
 		tree = dendropy.Tree.get(data=t, schema="newick")
-		# Add internal nodes and infer their states.
+
+		# Initialize array dict
 		array_dict = {}
+		for array in args.arrays_to_join:
+			array_dict[array] = CRISPR_mp.Array(array, array_spacers_dict[array], extant=True)
+		all_arrays = [array for array in array_dict.values()]
+
+		# Add internal nodes and infer their states.
 		for node in tree.seed_node.postorder_iter():
 			if node.taxon:
-				print(node.taxon.label)
+				a = node.taxon.label 
+				if len(node.sibling_nodes()) != 0:
+					sister = node.sibling_nodes()[0]
+					parent = node.parent_node
+					if sister.taxon:
+						b = sister.taxon.label
+						if not parent.taxon: # Only need to add the ancestor once.
+						
+							results = CRISPR_mp.resolve_pairwise_parsimony(array_dict[a], array_dict[b], all_arrays, array_dict, node_ids, node_count, tree, event_costs)
+							if results == "No_ID":
+								Incomplete_tree = True
+								break
+							else:
+								array_dict[a], array_dict[b], ancestor = results
+								node_count+=1
 
+							array_dict[ancestor.id] = ancestor
+							node.parent_node.taxon = taxon_namespace.get_taxon(ancestor.id)
+							node.edge_length = array_dict[a].distance
+							sister.edge_length = array_dict[b].distance
+
+		score = tree.length()
+		print(score)	
+		print(tree.as_ascii_plot(show_internal_node_labels=True))
+		print(tree.as_string("newick"))
 
 
 if __name__ == '__main__':
