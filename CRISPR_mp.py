59543b13def7adb6b86a5960e93d68c71b36814e
Alan Collins <alan.collins@bath.edu>
2021-08-31 11:21:04 -0500

    Tweaked spacer cartoon shape and outline thickness. Set acquisition, duplication, indel, and trailer loss emphasis to scale based on spacer width.

--- CRISPR_mp.py
+++ CRISPR_mp.py
@@ -1165,8 +1165,8 @@ def plot_tree(tree, array_dict, filename, spacer_cols_dict, branch_lengths=False
 	# hscale = (dim_x+1)/(tree_width + max([len(array.spacers) for array in array_dict.values()])) # Factor to scale all branch lengths to fit them in the plot
 	# vscale = (dim_y+1)/tree_height
 	branch_spacing = 2 # Space between branches in tree.
-	spacer_width = 0.2 # Thickness off spacer cartoons.
-	outline = 2 # Thickness of spacer_outline
+	spacer_width = 0.3 # Thickness off spacer cartoons.
+	outline = 3 # Thickness of spacer_outline
 	spacing = outline*0.15
 	spacer_size = 2.5
 	brlen_scale = 0.5
@@ -1400,8 +1400,8 @@ def plot_tree(tree, array_dict, filename, spacer_cols_dict, branch_lengths=False
 								
 							if diff_type.type == 'indel_gap' or diff_type.type == 'indel_mm' or diff_type.type == 'indel': 
 								if spacer == '-':
-									ax.plot([start_pos_x-spacer_size*spacer_count-spacing, start_pos_x-spacer_size*spacer_count-spacer_size+spacing],[start_pos_y+spacer_width+0.4, start_pos_y-spacer_width-0.4], color="#666666", linewidth=3, solid_capstyle="butt")
-									ax.plot([start_pos_x-spacer_size*spacer_count-spacing, start_pos_x-spacer_size*spacer_count-spacer_size+spacing],[start_pos_y-spacer_width-0.4, start_pos_y+spacer_width+0.4], color="#666666", linewidth=3, solid_capstyle="butt")
+									ax.plot([start_pos_x-spacer_size*spacer_count-spacing, start_pos_x-spacer_size*spacer_count-spacer_size+spacing],[start_pos_y+spacer_width+0.2, start_pos_y-spacer_width-0.2], color="#666666", linewidth=3, solid_capstyle="butt")
+									ax.plot([start_pos_x-spacer_size*spacer_count-spacing, start_pos_x-spacer_size*spacer_count-spacer_size+spacing],[start_pos_y-spacer_width-0.2, start_pos_y+spacer_width+0.2], color="#666666", linewidth=3, solid_capstyle="butt")
 									spacer_count+=1 # Shift future spacers a bit to make spacer for this line.
 								else:
 									nspacers = len([child.aligned[i] for i in diff_type.indices if child.aligned[i] != '-'])
@@ -1422,17 +1422,17 @@ def plot_tree(tree, array_dict, filename, spacer_cols_dict, branch_lengths=False
 								nspacers = len(diff_type.indices)
 
 								rcParams['path.sketch'] = (25, 60, 1)
-								ax.plot(np.linspace(start_pos_x-spacer_size*(spacer_count+nspacers)+spacing,start_pos_x-spacer_size*spacer_count-spacing,3),[start_pos_y-0.4]*3,color="#666666", linewidth=2, solid_capstyle="butt")
+								ax.plot(np.linspace(start_pos_x-spacer_size*(spacer_count+nspacers)+spacing,start_pos_x-spacer_size*spacer_count-spacing,3),[start_pos_y-spacer_width/2-0.4]*3,color="#666666", linewidth=2, solid_capstyle="butt")
 								rcParams['path.sketch'] = (0, 0, 0)
 
 							elif diff_type.type == "trailer_loss":
 								if spacer == '-': # Draw a single sloped line
-									ax.plot([start_pos_x-spacer_size*spacer_count-spacing, start_pos_x-spacer_size*spacer_count-spacer_size+spacing],[start_pos_y+0.5, start_pos_y-0.5], color="#666666", linewidth=3, solid_capstyle="butt")
+									ax.plot([start_pos_x-spacer_size*spacer_count-spacing, start_pos_x-spacer_size*spacer_count-spacer_size+spacing],[start_pos_y+spacer_width+0.2, start_pos_y-spacer_width-0.2], color="#666666", linewidth=3, solid_capstyle="butt")
 									spacer_count+=1 # Shift future spacers a bit to make spacer for this line.
 							elif diff_type.type == "duplication":
 								nspacers = len(diff_type.indices)
 
-								ax.plot(np.linspace(start_pos_x-spacer_size*(spacer_count+nspacers)+spacing,start_pos_x-spacer_size*spacer_count-spacing,3),[start_pos_y-0.5]*3,color="#666666", linewidth=3, solid_capstyle="butt")
+								ax.plot(np.linspace(start_pos_x-spacer_size*(spacer_count+nspacers)+spacing,start_pos_x-spacer_size*spacer_count-spacing,3),[start_pos_y-spacer_width/2-0.5]*3,color="#666666", linewidth=3, solid_capstyle="butt")
 								
 
 
