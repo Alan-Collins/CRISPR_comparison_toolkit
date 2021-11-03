18fc21fbd92d2f3d2ba14aa8de6c114f4a79bb64
Alan Collins <alan.collins@bath.edu>
2021-11-03 13:06:39 -0500

    update script paths

--- setup.py
+++ setup.py
@@ -8,10 +8,18 @@ setup(
         "matplotlib",
         "setuptools",
     ],
-    scripts=['CRISPR_mp_by_topology.py',
-        'CRISPR_nj.py',
-        'CRISPR_mp.py',
-        'CRtree_constrained.py',
-        'run_CRISPR_mp_all.py']
+    scripts=[
+        'CRISPR_comparison_toolkit/CRISPRtree.py',
+        "CRISPR_comparison_toolkit/CRISPRcasfinder2arrays.py",
+        "CRISPR_comparison_toolkit/CRISPRdiff.py",
+        "CRISPR_comparison_toolkit/CRISPRspacers2network.py",
+        "CRISPR_comparison_toolkit/CRISPRtree.py",
+        "CRISPR_comparison_toolkit/CRtree_constrained.py",
+        "CRISPR_comparison_toolkit/minced2arrays.py",
+        "CRISPR_comparison_toolkit/narbl2arrays.py",
+        "CRISPR_comparison_toolkit/process_CRISPR_blast.py",
+        "CRISPR_comparison_toolkit/Process_CRISPR_cas_finder_out.py",
+        "CRISPR_comparison_toolkit/reps2arrays.py",
+    ]
 )
 
