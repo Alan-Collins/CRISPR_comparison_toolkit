e4e0ba720260925707d78718bc96f05adb42f9be
Alan Collins <alan.collins@bath.edu>
2021-11-07 13:43:49 -0600

    Added scale_branches test setup and basic test

--- test/test_tree_operations.py
+++ test/test_tree_operations.py
@@ -4,9 +4,10 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import unittest
-from cctk import tree_operations
+import dendropy
+from CRISPR_comparison_toolkit.cctk import tree_operations
 
-class TestTreeOps(unittest.TestCase):
+class TestCreateNodeIds(unittest.TestCase):
 
 	def test_create_internal_node_ids(self):
 
@@ -31,6 +32,21 @@ class TestTreeOps(unittest.TestCase):
 			tree_operations.create_internal_node_ids(5.0)
 			tree_operations.create_internal_node_ids(5, prefix=10)
 
+
+class TestScaleBranches(unittest.TestCase):
+
+	def setUp(self):
+		self.tree = dendropy.Tree.get(
+			data="((A:1,B:2):3,(C:1,D:5):4);", 
+			schema="newick")
+
+	def test_scale_branch_lengths(self):
+
+		self.assertEqual(tree_operations.scale_branches(self.tree, 10)
+			.as_string("newick").strip(), 
+			"((A:2,B:4):6,(C:2,D:10):8):0;")
+
+		
 
 if __name__ == '__main__':
 	unittest.main()
