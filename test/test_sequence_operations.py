17d3f3aedc091c10c25db69f98543041b5e7f192
Alan Collins <alan.collins@bath.edu>
2021-11-09 16:20:01 -0600

    Added tests for Needle() with sequence and CRISPR arrays.

--- test/test_sequence_operations.py
+++ test/test_sequence_operations.py
@@ -27,7 +27,40 @@ class TestHamming(unittest.TestCase):
 		self.assertEqual(seqops.hamming("ATCG", "GATCGG"),
 			(6,6,1))
 
+class TestNeedle(unittest.TestCase):
 
+	def test_needle_with_similar_sequences(self):
+		self.assertEqual(seqops.needle(
+				"ATTG",
+				"ATCG",
+				match=1),
+			("ATTG", "ATCG"))
+
+	def test_needle_with_different_sequences(self):
+
+		self.assertEqual(seqops.needle(
+				"TTTTTTATCGTTTTTT",
+				"ATCG",
+				match=1),
+			("TTTTTTATCGTTTTTT", "------ATCG------"))
+
+		self.assertEqual(seqops.needle(
+				"ACTCGCTGCCTGTGA",
+				"CGATCACGATGTAGC",
+				match=1),
+			("-ACTCGCTGCCTGT-GA", "CGATCAC-G-ATGTAGC"))
+
+	def test_needle_with_CRISPR_arrays(self):
+
+		self.assertEqual(seqops.needle(
+				[1,2,3,4,5,6,7],
+				[8,9,10,11,12,5,7]),
+			(['-',1,2,3,4,5,6,7], [8,9,10,11,12,5,'-',7]))
+
+		self.assertEqual(seqops.needle(
+				[1,2,3,4,5,6,7],
+				[8,9,3,11,12,5,7]),
+			([1,2,3,'-',4,5,6,7], [8,9,3,11,12,5,'-',7]))
 
 
 
