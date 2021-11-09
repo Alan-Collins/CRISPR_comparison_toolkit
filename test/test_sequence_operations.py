f450e42b0c11aca220839ab851e705d0544e3521
Alan Collins <alan.collins@bath.edu>
2021-11-09 17:04:56 -0600

    Added Raise tests

--- test/test_sequence_operations.py
+++ test/test_sequence_operations.py
@@ -17,6 +17,10 @@ class TestRevComp(unittest.TestCase):
 			seqops.rev_comp("abracadabraNnN...#!/"),
 			"/!#...NnNtrbtdtgtrbt")
 
+	def test_rev_comp_raise(self):
+		with self.assertRaises(TypeError):
+			seqops.rev_comp(1_250_999)
+
 class TestHamming(unittest.TestCase):
 
 	def test_hamming_with_sequence(self):
@@ -26,6 +30,11 @@ class TestHamming(unittest.TestCase):
 
 		self.assertEqual(seqops.hamming("ATCG", "GATCGG"),
 			(6,6,1))
+
+	def test_hamming_raise(self):
+		with self.assertRaises(TypeError):
+			seqops.hamming(999, "ATCG")
+			seqops.needle("ATCG", 999)
 
 class TestNeedle(unittest.TestCase):
 
@@ -62,6 +71,13 @@ class TestNeedle(unittest.TestCase):
 				[8,9,3,11,12,5,7]),
 			([1,2,3,'-',4,5,6,7], [8,9,3,11,12,5,'-',7]))
 
+	def test_needle_raise(self):
+		with self.assertRaises(TypeError):
+			seqops.needle(999, "ATCG")
+			seqops.needle("ATCG", 999)
+			seqops.needle("ATTG", "ATCG", match="a")
+			seqops.needle("ATTG", "ATCG", mismatch="a")
+			seqops.needle("ATTG", "ATCG", gap="a")
 
 
 if __name__ == '__main__':
