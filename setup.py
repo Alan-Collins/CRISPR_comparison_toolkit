22327f51a9dfd39a4b2cfb71752d25335c76719c
Alan Collins <alan.collins@bath.edu>
2021-11-17 08:02:04 -0600

    Attempt to add cctk package install

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name="cctk",
+    author="Alan Collins",
+    license="GPL-3.0",
     install_requires=[
         "pip",
         "dendropy",
@@ -20,6 +22,7 @@ setup(
         "CRISPR_comparison_toolkit/process_CRISPR_blast.py",
         "CRISPR_comparison_toolkit/Process_CRISPR_cas_finder_out.py",
         "CRISPR_comparison_toolkit/reps2arrays.py",
-    ]
+    ],
+    packages = ["CRISPR_comparison_toolkit/cctk"]
 )
 
