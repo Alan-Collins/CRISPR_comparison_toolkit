15fdd48e1fdc93485f7333830380d090a5c19eb4
Alan Collins <alan.collins@bath.edu>
2022-01-05 14:29:52 -0600

    Replaced object for weighting event type probability from dict to list

--- CRISPR_comparison_toolkit/evolve_array.py
+++ CRISPR_comparison_toolkit/evolve_array.py
@@ -108,7 +108,7 @@ def check_outdir(outdir):
 	return outdir
 
 
-def tick(active_arrays, tree, tree_namespace, spacer_n, array_name, events_dict,
+def tick(active_arrays, tree, tree_namespace, spacer_n, array_name, events_list,
 	all_arrays, loss_rate):
 	"""Choose an event and return the resulting array.
 	
@@ -127,9 +127,9 @@ def tick(active_arrays, tree, tree_namespace, spacer_n, array_name, events_dict,
 		Next spacer ID to use.
 	  array_name (int):
 	    The ID to assign to newly create Array instances.
-	  events_dict (dict):
-	    Dict to look up to which event a selected random number
-	    corresponds
+	  events_list (list):
+	    List of events to choose from where events are repeated in list
+	    to control chance of their being selected.
 	  all_arrays (list):
 	    list of all Array instances
 	  loss_rate (int):
@@ -142,7 +142,7 @@ def tick(active_arrays, tree, tree_namespace, spacer_n, array_name, events_dict,
 	"""
 
 	source_array = random.choice(active_arrays)
-	event = events_dict[random.randint(1,len(events_dict))]
+	event = random.choice(events_list)
 
 	# Don't allow deletion if array is only 1 spacer long
 
@@ -275,29 +275,25 @@ def main(args):
 	spacer_n = 1 # Track ID of spacers
 	array_name = 0
 	active_arrays = []
-	events_dict = {}
+	events_list = []
 
 	init_array = array_parsimony.Array(str(array_name), extant=False)
 	array_name+=1
 
+	# Initialize starting array
 	for _ in range(args.initial_length):
 		init_array.spacers.insert(0,spacer_n)
 		spacer_n+=1
 	all_arrays = [init_array]
 	active_arrays = [init_array]
-	
 
-	i = 1
-	for _ in range(args.acquisition):
-		events_dict[i] = 'Acquisition'
-		i+=1
-	for _ in range(args.trailer_loss):
-		events_dict[i] = 'Trailer_loss'
-		i+=1
-	for _ in range(args.deletion):
-		events_dict[i] = 'Deletion'
-		i+=1
+	# build list of events where repetitions of event types controls
+	# chance that the event will be selected.
+	events_list += (['Acquisition']*args.acquisition
+		+ ['Trailer_loss']*args.trailer_loss
+		+ ['Deletion']*args.deletion)
 
+	# Initialize tree
 	tree_namespace = dendropy.TaxonNamespace(
 		[str(i) for i in range(args.num_events+1)])
 	tree = dendropy.Tree(taxon_namespace=tree_namespace)
@@ -306,16 +302,19 @@ def main(args):
 	first_node.taxon = tree_namespace.get_taxon('0')
 	tree.seed_node.add_child(first_node)
 
+	# Run evolution model
 	for _ in range(args.num_events):
 		active_arrays, tree, spacer_n, array_name, all_arrays = tick(
-			active_arrays, tree, tree_namespace, spacer_n, array_name, events_dict,
+			active_arrays, tree, tree_namespace, spacer_n, array_name, events_list,
 			all_arrays, args.loss_rate)
 
 	# for a in all_arrays:
 	# 	print(str(a.id) + '\t' + ' '.join([str(i) for i in a.spacers]))
 
+	# Remove intermediate nodes from tree
 	new_tree = summarise_tree(tree)
 
+	# Add arbitrary branch lengths for plotting purposes.
 	for node in new_tree.postorder_node_iter():
 		# Skip seed node and its child
 		if node == new_tree.seed_node:		 
@@ -330,7 +329,6 @@ def main(args):
 	# print(new_tree.as_ascii_plot(show_internal_node_labels=True))
 
 	# Plot tree
-
 	active_spacers = []
 	for array in active_arrays:
 		active_spacers += array.spacers
@@ -369,12 +367,6 @@ def main(args):
 		dpi=dpi, line_scale=line_scale, branch_lengths=args.brlen_labels,
 		branch_spacing=branch_spacing, brlen_scale=brlen_scale,
 		label_text_size=label_text_size, annot_text_size=annot_text_size)
-
-
-
-
-	
-
 
 if __name__ == '__main__':
 	args = cmdline_args()
