c74f5405e33e498cf599f96b0ca77a59f46fcdf9
Alan Collins <alan.collins@bath.edu>
2022-01-28 10:51:09 -0600

    Remove dead column from array file

--- CRISPR_comparison_toolkit/cctkpkg/file_handling.py
+++ CRISPR_comparison_toolkit/cctkpkg/file_handling.py
@@ -34,7 +34,7 @@ def read_array_file(file):
 	with open(file, 'r') as fin:
 		for line in fin.readlines():
 			bits = line.split()
-			array_spacers_dict[bits[0]] = bits[2:]
+			array_spacers_dict[bits[0]] = bits[1:]
 	return array_spacers_dict
 
 
