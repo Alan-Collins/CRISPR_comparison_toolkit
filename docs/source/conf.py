7c6d3c0d1e3a12df8ebe4de317934693ad763197
Alan Collins <alan.collins@bath.edu>
2022-02-10 11:55:50 -0600

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Alan Collins'
 author = 'Alan Collins'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
