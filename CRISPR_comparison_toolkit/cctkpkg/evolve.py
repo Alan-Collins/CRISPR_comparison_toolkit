fedefc4eb4ac02a7c9c12e1d4e010808527b1eb4
Alan Collins <alan.collins@bath.edu>
2022-02-16 09:08:50 -0600

    standardize random.choice use

--- CRISPR_comparison_toolkit/cctkpkg/evolve.py
+++ CRISPR_comparison_toolkit/cctkpkg/evolve.py
@@ -211,7 +211,7 @@ def tick(active_arrays, tree, tree_namespace, spacer_n, array_name, events_list,
 
 	if len(source_array.spacers) == 1:
 		while event == "Deletion":
-			event = events_list[random.randint(1,len(events_list))]
+			event = random.choice(events_list)
 
 	array = array_parsimony.Array(str(array_name), 
 		spacers=[i for i in source_array.spacers],
