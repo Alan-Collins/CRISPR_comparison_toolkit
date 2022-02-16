211bc872b6033a09f0f882f88bac341794ac8e31
Alan Collins <alan.collins@bath.edu>
2022-02-16 09:04:39 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cctk",
-    version="0.5.1",
+    version="0.5.2",
     author="Alan Collins",
     autor_email="alan.collins@bath.edu",
     url="https://github.com/Alan-Collins/CRISPR_comparison_toolkit",
